052a0b0704fccb4d0213b5af2a53f76afb1145b2
Foeifoei <110921560+foeifoei@users.noreply.github.com>
2022-09-02 17:45:55 +0700

    Menu Food Selection For you

---
+++ mushroomshop.py
@@ -0,0 +1,19 @@
+b = ("Hello My Customer , I'm Mushroomshop")
+h = 1
+for i in  b:
+    print(i)
+
+Myorder1 = ["red mushroom" , "original mushroom" , "spicy mushroom" , "mala mushroom"]
+Myorder2 = ["Beer" , "sauce" , "ice cube" , "Coke"]
+
+customer = "wine"
+
+if customer in Myorder1 :
+    print("mushroom")
+elif customer in Myorder2 :
+    print("Beverage")
+
+else :
+    print("No products,Please enter again")
+
+print("Thank you for your support")
